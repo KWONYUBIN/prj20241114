59f5c7408d39b902974a28d97510830134811f46
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-18 10:41:23 +0900

    회원 탈퇴

--- frontend/src/page/member/MemberInfo.jsx
+++ frontend/src/page/member/MemberInfo.jsx
@@ -1,17 +1,60 @@
 import { Box, Input, Spinner, Stack, Textarea } from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Field } from "../../components/ui/field.jsx";
+import { Button } from "../../components/ui/button.jsx";
+import {
+  DialogActionTrigger,
+  DialogBody,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "../../components/ui/dialog.jsx";
+import { toaster } from "../../components/ui/toaster.jsx";
 
 export function MemberInfo() {
   const [member, setMember] = useState(null);
+  const [password, setPassword] = useState("");
+  const [open, setOpen] = useState(false);
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     // 회원정보 얻기
     axios.get(`/api/member/${id}`).then((res) => setMember(res.data));
   }, []);
+
+  function handleDeleteClick() {
+    axios
+      .delete("/api/member/remove", {
+        data: { id, password },
+      })
+      .then((res) => {
+        const message = res.data.message;
+
+        toaster.create({
+          type: message.type,
+          description: message.text,
+        });
+        navigate("/member/signup");
+      })
+      .catch((e) => {
+        const message = e.response.data.message;
+
+        toaster.create({
+          type: message.type,
+          description: message.text,
+        });
+      })
+      .finally(() => {
+        setOpen(false);
+        setPassword("");
+      });
+  }
 
   if (!member) {
     return <Spinner />;
@@ -33,6 +76,37 @@ export function MemberInfo() {
         <Field label={"가입일시"}>
           <Input type={"datetime-local"} readOnly value={member.inserted} />
         </Field>
+        <Box>
+          <DialogRoot open={open} onOpenChange={(e) => setOpen(e.open)}>
+            <DialogTrigger asChild>
+              <Button colorPalette={"red"}>탈퇴</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>탈퇴 확인</DialogTitle>
+              </DialogHeader>
+              <DialogBody>
+                <Stack gap={5}>
+                  <Field label={"암호"}>
+                    <Input
+                      placeholder={"암호를 입력해주세요."}
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Field>
+                </Stack>
+              </DialogBody>
+              <DialogFooter>
+                <DialogActionTrigger>
+                  <Button variant={"outline"}>취소</Button>
+                </DialogActionTrigger>
+                <Button colorPalette={"red"} onClick={handleDeleteClick}>
+                  탈퇴
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </DialogRoot>
+        </Box>
       </Stack>
     </Box>
   );
