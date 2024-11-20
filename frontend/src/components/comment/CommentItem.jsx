2479998c9d459e98f901726a866adde0ce7e3c16
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-20 14:18:19 +0900

    댓글 삭제 dialog 추가

--- frontend/src/components/comment/CommentItem.jsx
+++ frontend/src/components/comment/CommentItem.jsx
@@ -1,26 +1,67 @@
-import { Box, Group, Textarea } from "@chakra-ui/react";
+import { Box, Flex, HStack } from "@chakra-ui/react";
 import { Button } from "../ui/button.jsx";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthenticationContext } from "../context/AuthenticationProvider.jsx";
+import {
+  DialogActionTrigger,
+  DialogBody,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog.jsx";
 
-export function CommentInput({ boardId, onSaveClick }) {
-  const [comment, setComment] = useState("");
+function DeleteButton({ onClick }) {
+  const [open, setOpen] = useState(false);
 
   return (
-    <Box>
-      <Group>
-        <Textarea
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-        <Button
-          onClick={() => {
-            setComment("");
-            onSaveClick(comment);
-          }}
-        >
-          댓글 쓰기
-        </Button>
-      </Group>
-    </Box>
+    <>
+      <DialogRoot open={open} onOpenChange={(e) => setOpen(e.open)}>
+        <DialogTrigger asChild>
+          <Button colorPalette={"red"}>삭제</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>삭제 확인</DialogTitle>
+          </DialogHeader>
+          <DialogBody>
+            <p>댓글을 삭제하시겠습니까?</p>
+          </DialogBody>
+          <DialogFooter>
+            <DialogActionTrigger>
+              <Button variant={"outline"}>취소</Button>
+            </DialogActionTrigger>
+            <Button colorPalette={"red"} onClick={onClick}>
+              삭제
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </DialogRoot>
+    </>
+  );
+}
+
+export function CommentItem({ comment, onDeleteClick }) {
+  const { hasAccess } = useContext(AuthenticationContext);
+
+  return (
+    <HStack border={"1px solid black"} m={5}>
+      <Box flex={1}>
+        <Flex justify={"space-between"}>
+          <h3>{comment.memberId}</h3>
+          <h4>{comment.inserted}</h4>
+        </Flex>
+        <p>{comment.comment}</p>
+      </Box>
+      {hasAccess(comment.memberId) && (
+        <Box>
+          <Button colorPalette={"purple"}>수정</Button>
+
+          <DeleteButton onClick={() => onDeleteClick(comment.id)} />
+        </Box>
+      )}
+    </HStack>
   );
 }
